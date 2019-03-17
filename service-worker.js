d13f5dc32c2f5503ccf38228975968a69d67840e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-17 08:06:41 +0000

    Deploy maharshmellow/maharshmellow.github.io to github.com/maharshmellow/maharshmellow.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.2e06de576a821336c147694e874be8bc.js"
+  "/precache-manifest.17b8f158d7b3c4eba312a068f91fcc41.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
