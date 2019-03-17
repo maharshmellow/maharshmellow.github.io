59e8946219e2e71f3470a2472a805c1e7b82be8b
Maharsh Patel <patel@maharsh.net>
2019-03-17 02:33:41 -0600

    fixed styling issues on mobile

--- src/data/Work.jsx
+++ src/data/Work.jsx
@@ -4,7 +4,7 @@ const workData = [
         experiences: [
             {
                 title: "Software Engineering Intern",
-                date: "Mar 2018 - Jun 2018",
+                date: "Mar '18 - Jun '18",
                 location: "Mountain View, CA",
                 description: "## - Applied Machine Learning models to enable Human Computer Interaction on a future product \n ## - Worked with C++ and Tensorflow to extract body keypoints from a live input stream, and enabled low latency, real-time analysis using ML accelerators \n ## - Used Typescript to create a canvas game to demonstrate the high accuracy and low latency interaction that the internship project can enable"
             }
@@ -15,7 +15,7 @@ const workData = [
         experiences: [
             {
                 title: "Software Developer Co-op",
-                date: "Jun 2018 - Apr 2019",
+                date: "Jun '18 - Apr '19",
                 location: "Edmonton, AB",
                 description: "## - Working with React to develop smoother onboarding experiences for new Quickbooks Online customers \n ## - Implemented a mandatory in-product phone collection dialog to increase the security profile of every 1 in 10 at-risk customers \n ## - Created an in-product password verification system to protect access from SSO users to pages containing sensitive data \n ## - Implemented a Visual Regression testing framework to enable the detection of small visual differences between iterations \n ## - Gained experience with unit and automated system testing, as well as A/B testing on live users through feature experiments"
             }
@@ -26,19 +26,19 @@ const workData = [
         experiences: [
             {
                 title: "Research Assistant",
-                date: "Sep 2017 - Mar 2018",
+                date: "Sep '17 - Mar '18",
                 location: "Edmonton, AB",
                 description: "## - Converted the Basic Concepts Classification into Linked Open Data using Python"
             },
             {
                 title: "Metadata Assistant",
-                date: "May 2017 - Sep 2018",
+                date: "May '17 - Sep '18",
                 location: "Edmonton, AB",
                 description: "## - Created a Django web application that Universities across Canada will use to convert their library metadata into Linked Data \n ## - Created a SPARQL endpoint to query the Library of Congress and University metadata \n ## - Gained experience with Linked Data, MARC, SPARQL, RDF, Apache Jena Fuseki, and Virtuoso"
             },
             {
                 title: "Summer Research Intern",
-                date: "Jun 2014 - Aug 2014",
+                date: "Jun '14 - Aug '14",
                 location: "Edmonton, AB",
                 description: "## - Programmed a joystick to control an iRobot Create’s motion using Python and C \n ## - Implemented and modified existing reinforcement learning algorithms \n ## - Successfully made a robot predict its future collision in the test environment"
             }
